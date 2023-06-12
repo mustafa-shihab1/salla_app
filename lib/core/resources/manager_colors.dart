fbd5f0b19da8674d70648529443bbdcbe1865da2
mstfa_201 <mostafaa.sh2001@gmail.com>
2023-06-12 15:43:12 +0300

    add colors

---
+++ lib/core/resources/manager_colors.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class ManagerColors {
+  static const Color white = Colors.white;
+  static const Color black = Colors.black;
+  static const Color logoColor = Colors.indigoAccent;
+  static const Color textColorLight = Color(0xff525167);
+  static const Color textColorDark = Colors.white;
+
+}
