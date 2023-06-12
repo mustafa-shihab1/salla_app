d35b83b71f79304c22a4ad8af3f4200185f8f0a0
mstfa_201 <mostafaa.sh2001@gmail.com>
2023-06-12 19:34:43 +0300

    Create splash_view.dart

---
+++ lib/features/splash/presentation/view/splash_view.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:home_service_application/core/resources/manager_assets.dart';
+import 'package:home_service_application/core/resources/manager_colors.dart';
+
+class SplashView extends StatelessWidget {
+  const SplashView({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body:Center(
+        child: SvgPicture.asset(ManagerAssets.logo,color: ManagerColors.logoColor,),
+      ),
+    );
+  }
+}
