510313c97dcd4615c6f6ac6445013820ef067477
mstfa_201 <mostafaa.sh2001@gmail.com>
2023-06-12 15:43:31 +0300

    add fonts

---
+++ lib/core/resources/manager_fonts.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:home_service_application/core/util/size_util.dart';
+
+class ManagerFontFamily {
+  static const String fontFamily = 'NunitoSans';
+}
+
+class ManagerFontWeight {
+  static const FontWeight bold = FontWeight.w700;
+  static const FontWeight regular = FontWeight.w400;
+  static const FontWeight medium = FontWeight.w500;
+  static const FontWeight w200 = FontWeight.w200;
+  static const FontWeight w300 = FontWeight.w300;
+}
+
+class ManagerFontSize {
+  static double s2 = SizeUtil.setFontSize(2);
+  static double s4 = SizeUtil.setFontSize(4);
+  static double s6 = SizeUtil.setFontSize(6);
+  static double s8 = SizeUtil.setFontSize(8);
+  static double s10 = SizeUtil.setFontSize(10);
+  static double s12 = SizeUtil.setFontSize(12);
+  static double s14 = SizeUtil.setFontSize(14);
+  static double s16 = SizeUtil.setFontSize(16);
+  static double s18 = SizeUtil.setFontSize(18);
+  static double s20 = SizeUtil.setFontSize(20);
+  static double s22 = SizeUtil.setFontSize(22);
+  static double s24 = SizeUtil.setFontSize(24);
+  static double s34 = SizeUtil.setFontSize(34);
+}
