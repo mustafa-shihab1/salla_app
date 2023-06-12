80e9944faa7ee686fada17fc11a0dc6252df4659
mstfa_201 <mostafaa.sh2001@gmail.com>
2023-06-12 15:43:04 +0300

    add assets

---
+++ lib/core/resources/manager_assets.dart
@@ -0,0 +1,9 @@
+const String imagesPath = 'assets/images/';
+
+class ManagerAssets {
+  static const String logo = '$imagesPath/salla_logo.svg';
+  static const String onBoardingImg1 = '$imagesPath/onBoard_1.jpg';
+  static const String onBoardingImg2 = '$imagesPath/onBoard_2.jpg';
+  static const String onBoardingImg3 = '$imagesPath/onBoard_3.jpg';
+
+}
