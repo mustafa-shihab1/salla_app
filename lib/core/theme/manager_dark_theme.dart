10fa2cb73eda3aa95d4db5f51438a7e31c71f650
mstfa_201 <mostafaa.sh2001@gmail.com>
2023-06-12 15:47:16 +0300

    dark theme - working on it

---
+++ lib/core/theme/manager_dark_theme.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+import 'package:home_service_application/core/resources/manager_text_theme_dark.dart';
+
+ThemeData managerDarkThemeData() {
+  return ThemeData.dark().copyWith(
+    useMaterial3: true,
+
+    /// MAIN COLORS APP
+    //primaryColor: ManagerColors.primaryColor,
+    //primaryColorDark: ManagerColors.primaryColorDark,
+
+    // this for disabled button colors
+    //disabledColor: ManagerColors.disabledColor,
+    //splashColor: ManagerColors.greyLight,
+    //scaffoldBackgroundColor: ManagerColors.scaffoldBackgroundColorDark,
+
+    /// APP BAR THEME
+    appBarTheme: AppBarTheme(
+      centerTitle: true,
+      //color: ManagerColors.greyLight,
+      //elevation: Constants.elevationAppBar,
+      // titleTextStyle: getBoldTextStyle(
+      //   fontSize: ManagerFontSize.s14,
+      //   color: ManagerColors.primaryColor,
+      // ),
+    ),
+
+    /// BUTTON THEME
+    buttonTheme: const ButtonThemeData(
+      shape: RoundedRectangleBorder(),
+      // disabledColor: ManagerColors.greyLight,
+      // buttonColor: ManagerColors.buttonColorDarK,
+    ),
+
+    textTheme: ManagerTextThemeDark(),
+  );
+}
