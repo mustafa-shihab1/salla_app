1a49f072dd82df73624e981c5dde5a757fbb9d18
mstfa_201 <mostafaa.sh2001@gmail.com>
2023-06-15 21:30:44 +0300

    add route for loginView

--- lib/routes/routes.dart
+++ lib/routes/routes.dart
@@ -2,12 +2,14 @@
 import 'package:flutter/material.dart';
 import 'package:home_service_application/config/dependency_injection.dart';
 import 'package:home_service_application/core/resources/manager_strings.dart';
-import 'package:home_service_application/features/on_boarding/presentation/on_boarding_view.dart';
+import 'package:home_service_application/features/auth/presentation/view/login_view.dart';
+import 'package:home_service_application/features/on_boarding/presentation/view/on_boarding_view.dart';
 import 'package:home_service_application/features/splash/presentation/view/splash_view.dart';
 
 class Routes {
   static const String splashView = '/splash_view';
   static const String onBoardingView = '/on_boarding_view';
+  static const String loginView = '/login_view';
 
 }
 
@@ -21,6 +23,9 @@ class RouteGenerator {
       case Routes.onBoardingView:
         initOnBoarding();
         return MaterialPageRoute(builder: (_) => const OnBoardingView());
+      case Routes.loginView:
+        initLoginModule();
+        return MaterialPageRoute(builder: (_) => LoginView());
       default:
         return unDefinedRoute();
     }
