438354c2f26cd725c404c34080f3f9ab29d25086
mstfa_201 <mostafaa.sh2001@gmail.com>
2023-06-15 21:26:40 +0300

    Update manager_colors.dart

--- lib/core/resources/manager_colors.dart
+++ lib/core/resources/manager_colors.dart
@@ -3,8 +3,12 @@ import 'package:flutter/material.dart';
 class ManagerColors {
   static const Color white = Colors.white;
   static const Color black = Colors.black;
-  static const Color logoColor = Colors.indigoAccent;
+  static const Color primaryColor = Colors.indigoAccent;
   static const Color textColorLight = Color(0xff525167);
   static const Color textColorDark = Colors.white;
+  static const Color boldGrey = Color(0xff424242);
+  static const Color grey = Colors.grey;
+  static const Color transparent = Colors.transparent;
+
 
 }
