5ad52f0fe0629acf32c2fbf81f6e018b2b2c83e6
mstfa_201 <mostafaa.sh2001@gmail.com>
2023-06-15 21:25:55 +0300

    add constants

--- lib/config/constants.dart
+++ lib/config/constants.dart
@@ -2,5 +2,11 @@ class Constants {
   static const double deviceWidth = 412;
   static const double deviceHeight = 869;
   static const int splashDuration = 3;
+  static const int onBoardingDurationTime = 1;
+  static const double elevationAppBar = 0;
 
+}
+
+class ConstantsPrefsKeys {
+  static const String onBoardingViewedKey = 'on_boarding_viewed';
 }
