a7a0325a6017aac0b49139c047c34de2916ee33a
mstfa_201 <mostafaa.sh2001@gmail.com>
2023-06-15 21:27:10 +0300

    add onBoarding strings

--- lib/core/resources/manager_strings.dart
+++ lib/core/resources/manager_strings.dart
@@ -1,5 +1,10 @@
 class ManagerStrings {
   static const String salla = 'Salla';
   static const String noRoutFound = 'No Rout Found';
-
+  static const String onBoardingTitle1 = 'Welcome to Our Store';
+  static const String onBoardingTitle2 = 'Explore a Vast Selection';
+  static const String onBoardingTitle3 = 'Seamless Shopping Experience';
+  static const String onBoardingSubTitle1 = 'Discover a world of shopping at your fingertips';
+  static const String onBoardingSubTitle2 = 'Find the perfect products that suit your style\nand needs';
+  static const String onBoardingSubTitle3 = 'Shop with ease, convenience, and secure transactions';
 }
