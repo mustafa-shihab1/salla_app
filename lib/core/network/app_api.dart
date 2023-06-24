aab1a66bbd5d56158405b06b7303967643892b46
mstfa_201 <mostafaa.sh2001@gmail.com>
2023-06-24 23:05:16 +0300

    Create app_api.dart

---
+++ lib/core/network/app_api.dart
@@ -0,0 +1,13 @@
+import 'package:retrofit/http.dart';
+import '../../config/constants.dart';
+import 'package:dio/dio.dart';
+
+part 'app_api.g.dart';
+
+@RestApi(baseUrl: ApiConstants.baseUrl)
+abstract class AppApi {
+  factory AppApi(
+      Dio dio, {
+        String baseUrl,
+      }) = _AppApi;
+}
