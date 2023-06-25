01c64be8681aaa5d43ca737aa94f36c7efb13f76
mstfa_201 <mostafaa.sh2001@gmail.com>
2023-06-25 13:38:41 +0300

    modify app_api

--- lib/core/network/app_api.dart
+++ lib/core/network/app_api.dart
@@ -2,6 +2,8 @@ import 'package:retrofit/http.dart';
 import '../../config/constants.dart';
 import 'package:dio/dio.dart';
 
+import '../../features/auth/data/response/login_response.dart';
+
 part 'app_api.g.dart';
 
 @RestApi(baseUrl: ApiConstants.baseUrl)
@@ -10,4 +12,9 @@ abstract class AppApi {
       Dio dio, {
         String baseUrl,
       }) = _AppApi;
+
+  @POST(RequestConstants.login)
+  Future<LoginResponse> login(
+      @Field(ApiConstants.email) email,
+      @Field(ApiConstants.password) password);
 }
--- lib/core/network/app_api.g.dart
+++ lib/core/network/app_api.g.dart
@@ -20,6 +20,39 @@ class _AppApi implements AppApi {
 
   String? baseUrl;
 
+  @override
+  Future<LoginResponse> login(
+    dynamic email,
+    dynamic password,
+  ) async {
+    const _extra = <String, dynamic>{};
+    final queryParameters = <String, dynamic>{};
+    final _headers = <String, dynamic>{};
+    final _data = {
+      'email': email,
+      'password': password,
+    };
+    final _result = await _dio
+        .fetch<Map<String, dynamic>>(_setStreamType<LoginResponse>(Options(
+      method: 'POST',
+      headers: _headers,
+      extra: _extra,
+    )
+            .compose(
+              _dio.options,
+              '/api/auth/user/login',
+              queryParameters: queryParameters,
+              data: _data,
+            )
+            .copyWith(
+                baseUrl: _combineBaseUrls(
+              _dio.options.baseUrl,
+              baseUrl,
+            ))));
+    final value = LoginResponse.fromJson(_result.data!);
+    return value;
+  }
+
   RequestOptions _setStreamType<T>(RequestOptions requestOptions) {
     if (T != dynamic &&
         !(requestOptions.responseType == ResponseType.bytes ||
