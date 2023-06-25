9f81d4e48b8f59d625ab73bbefb44272dfce5e9d
mstfa_201 <mostafaa.sh2001@gmail.com>
2023-06-25 13:39:17 +0300

    create base-response

---
+++ lib/core/base_response/base_response.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/cupertino.dart';
+import 'package:home_service_application/config/constants.dart';
+import 'package:json_annotation/json_annotation.dart';
+
+part 'base_response.g.dart';
+
+@JsonSerializable()
+class BaseResponse {
+  @JsonKey(name: ApiConstants.message)
+  String? message;
+}
