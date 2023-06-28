edb1d2ba4a598ab4d5b6cd6db75faf8db9bb1282
mstfa_201 <mostafaa.sh2001@gmail.com>
2023-06-28 22:57:19 +0300

    Update app_settings_shared_preferences.dart

--- lib/core/storage/local/app_settings_shared_preferences.dart
+++ lib/core/storage/local/app_settings_shared_preferences.dart
@@ -22,11 +22,36 @@ class AppSettingsSharedPreferences {
   }
 
   Future<void> setToken(String token) async {
+    await setLoggedIn();
     await _sharedPreferences.setString(ConstantsPrefsKeys.token, token);
   }
 
   String getToken() {
     return _sharedPreferences.getString(ConstantsPrefsKeys.token).onNull();
+  }
+
+  Future<void> setEmail(String email) async {
+    await _sharedPreferences.setString(ConstantsPrefsKeys.email, email);
+  }
+
+  String getEmail() {
+    return _sharedPreferences.getString(ConstantsPrefsKeys.email).onNull();
+  }
+
+  Future<void> setPassword(String password) async {
+    await _sharedPreferences.setString(ConstantsPrefsKeys.password, password);
+  }
+
+  String getPassword() {
+    return _sharedPreferences.getString(ConstantsPrefsKeys.password).onNull();
+  }
+
+  Future<void> setLoggedIn() async {
+    await _sharedPreferences.setBool(ConstantsPrefsKeys.loggedIn, true);
+  }
+
+  bool loggedIn() {
+    return _sharedPreferences.getBool(ConstantsPrefsKeys.loggedIn).onNull();
   }
 
   void clear() {
