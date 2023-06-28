e3ef99a7cb10aec9414e41e1cfedec92d0983c6c
mstfa_201 <mostafaa.sh2001@gmail.com>
2023-06-28 22:59:04 +0300

    edit login response

--- lib/features/auth/data/response/login_response.dart
+++ lib/features/auth/data/response/login_response.dart
@@ -1,13 +1,16 @@
+import 'package:home_service_application/config/constants.dart';
+import 'package:home_service_application/core/base_response/base_response.dart';
+import 'package:home_service_application/features/auth/data/response/login_data_response.dart';
 import 'package:json_annotation/json_annotation.dart';
 
 part 'login_response.g.dart';
 
 @JsonSerializable()
-class LoginResponse {
-  @JsonKey(name: 'token')
-  String? token;
+class LoginResponse extends BaseResponse{
+  @JsonKey(name: 'data')
+  LoginDataResponse? data;
 
-  LoginResponse(this.token);
+  LoginResponse({this.data});
 
   factory LoginResponse.fromJson(Map<String, dynamic> json) =>
       _$LoginResponseFromJson(json);
