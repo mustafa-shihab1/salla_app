a03402ad68b010145572c1553699cd820f7f70b9
mstfa_201 <mostafaa.sh2001@gmail.com>
2023-06-28 22:55:14 +0300

    add login constants

--- lib/config/constants.dart
+++ lib/config/constants.dart
@@ -1,4 +1,4 @@
-const String basePath = '/api';
+//const String basePath = '/api';
 
 class Constants {
   static const double deviceWidth = 412;
@@ -6,22 +6,29 @@ class Constants {
   static const int splashDuration = 3;
   static const int onBoardingDurationTime = 1;
   static const double elevationAppBar = 0;
-
+  static const int loginFirstPartFlex = 1;
+  static const int loginSecondPartFlex = 3;
+  static const double getStateWidgetRenderStateElevation = 3;
+  static const int loginTimer = 1;
 }
 
 class RequestConstants {
-  static const String login = '$basePath/login';
+  static const String login = 'login';//'$basePath/login';
 }
 class ConstantsPrefsKeys {
   static const String onBoardingViewedKey = 'on_boarding_viewed';
   static const String token = 'token';
+  static const String email = 'email';
+  static const String password = 'password';
+  static const String loggedIn = 'logged_in';
 }
 
 class ApiConstants {
-  static const String baseUrl = 'https://student.valuxapps.com';
+  static const String baseUrl = 'https://student.valuxapps.com/api/';
   static const String authorization = 'authorization';
   static const int sendTimeOutDuration = 120;
   static const int receiveTimeOutDuration = 120;
+  static const String token = 'token';
   static const String email = 'email';
   static const String password = 'password';
   static const String message = 'message';
@@ -29,4 +36,7 @@ class ApiConstants {
   static const String error = 'error';
   static const String badRequest = 'Bad Request';
   static const String noInternetConnection = 'No Internet connection';
+  static const String acceptLanguage = 'Accept-Language';
+  static const String english = 'en';
+  static const String arabic = 'ar';
 }
