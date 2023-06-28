42625b5711987d1c3c5aab2fb641144853e54788
mstfa_201 <mostafaa.sh2001@gmail.com>
2023-06-28 22:55:44 +0300

    inject login

--- lib/config/dependency_injection.dart
+++ lib/config/dependency_injection.dart
@@ -5,6 +5,8 @@ import 'package:get_it/get_it.dart';
 import 'package:home_service_application/core/network/app_api.dart';
 import 'package:home_service_application/core/network/dio_factory.dart';
 import 'package:home_service_application/core/storage/local/app_settings_shared_preferences.dart';
+import 'package:home_service_application/features/auth/data/data_source/remote_login_data_source.dart';
+import 'package:home_service_application/features/auth/data/repository_impl/login_repository_impl.dart';
 import 'package:home_service_application/features/auth/presentation/controller/login_controller.dart';
 import 'package:home_service_application/features/on_boarding/presentation/controller/on_boarding_controller.dart';
 import 'package:home_service_application/features/splash/presentation/controller/splash_controller.dart';
@@ -12,6 +14,8 @@ import 'package:internet_connection_checker_plus/internet_connection_checker_plu
 import 'package:shared_preferences/shared_preferences.dart';
 
 import '../core/internet_checker/internet_checker.dart';
+import '../features/auth/domain/repository/login_repository.dart';
+import '../features/auth/domain/use_case/login_use_case.dart';
 
 final instance = GetIt.instance;
 
@@ -62,5 +66,29 @@ disposeOnBoarding() {
 initLoginModule() {
   disposeSplash();
   disposeOnBoarding();
+  if (!GetIt.I.isRegistered<RemoteLoginDataSource>()) {
+    instance.registerLazySingleton<RemoteLoginDataSource>(
+          () => RemoteLoginDataSourceImplement(
+        instance<AppApi>(),
+      ),
+    );
+  }
+
+  if (!GetIt.I.isRegistered<LoginRepository>()) {
+    instance.registerLazySingleton<LoginRepository>(
+          () => LoginRepositoryImpl(
+        instance(),
+        instance(),
+      ),
+    );
+  }
+
+  if (!GetIt.I.isRegistered<LoginUseCase>()) {
+    instance.registerFactory<LoginUseCase>(
+          () => LoginUseCase(
+        instance<LoginRepository>(),
+      ),
+    );
+  }
   Get.put<LoginController>(LoginController());
 }
