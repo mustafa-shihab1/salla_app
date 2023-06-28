18deeabfcef16443c7aa6d5dc2956ead860f6ec4
mstfa_201 <mostafaa.sh2001@gmail.com>
2023-06-28 22:57:03 +0300

    add resources

--- lib/core/resources/manager_strings.dart
+++ lib/core/resources/manager_strings.dart
@@ -8,4 +8,18 @@ class ManagerStrings {
   static const String onBoardingSubTitle2 = 'Find the perfect products that suit your style\nand needs';
   static const String onBoardingSubTitle3 = 'Shop with ease, convenience, and secure transactions';
   static const String bearer = 'Bearer';
+  static const String login = 'Login';
+  static const String email = 'Email';
+  static const String invalidEmail = 'Invalid Email';
+  static const String invalidPassword = 'invalid Password';
+  static const String password = 'Password';
+  static const String rememberMe = 'Remember Me';
+  static const String loginDescription = 'login now to check out hot offers';
+  static const String signup = 'Sign up';
+  static const String notHaveAccount = 'Don\'t have an account?';
+  static const String forgotPassword = 'Forgot Password ?';
+  static const String loading = 'Loading...';
+  static const String ok = 'Ok';
+  static const String thanks = 'Thanks';
+
 }
