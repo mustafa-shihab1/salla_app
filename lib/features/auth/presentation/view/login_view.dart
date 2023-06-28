b1cf3f02fdc5bb3f181d0f217d65a40d3a18c15f
mstfa_201 <mostafaa.sh2001@gmail.com>
2023-06-28 22:58:26 +0300

    Update login_view.dart

--- lib/features/auth/presentation/view/login_view.dart
+++ lib/features/auth/presentation/view/login_view.dart
@@ -1,10 +1,179 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:home_service_application/core/resources/manager_colors.dart';
+import 'package:home_service_application/core/resources/manager_fonts.dart';
+import 'package:home_service_application/core/resources/manager_sizes.dart';
+import 'package:home_service_application/core/resources/manager_strings.dart';
+import 'package:home_service_application/core/resources/manager_styles.dart';
+import 'package:home_service_application/core/validator/validator.dart';
+import 'package:home_service_application/core/widgets/base_text_form_field.dart';
+import 'package:home_service_application/core/widgets/header_widget.dart';
+import 'package:home_service_application/core/widgets/main_button.dart';
+import 'package:home_service_application/features/auth/presentation/controller/login_controller.dart';
 
 class LoginView extends StatelessWidget {
-  const LoginView({Key? key}) : super(key: key);
+  final FieldValidator _failedValidator = FieldValidator();
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return GetBuilder<LoginController>(
+      builder: (controller) {
+        return Scaffold(
+          body: Form(
+            key: controller.formKey,
+            child: SingleChildScrollView(
+              child: Column(
+                children: [
+                  Container(height: 200, child: HeaderWidget(200)),
+                  Container(
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadiusDirectional.only(
+                        topStart: Radius.circular(
+                          ManagerRadius.r44,
+                        ),
+                        topEnd: Radius.circular(
+                          ManagerRadius.r44,
+                        ),
+                      ),
+                    ),
+                    padding: EdgeInsets.symmetric(
+                      horizontal: ManagerWidth.w16,
+                      vertical: ManagerHeight.h40,
+                    ),
+                    width: double.infinity,
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          ManagerStrings.login,
+                          style: getBoldTextStyle(
+                            fontSize: ManagerFontSize.s34,
+                            color: ManagerColors.black,
+                          ),
+                        ),
+                        const SizedBox(height: 3,),
+                        Container(
+                          width: ManagerWidth.w80,
+                          height: ManagerHeight.h4,
+                          decoration: const BoxDecoration(
+                            color: ManagerColors.primaryColor,
+                            borderRadius: BorderRadiusDirectional.only(
+                              bottomStart: Radius.circular(25),
+                              bottomEnd: Radius.circular(25),
+                            ),
+
+                          ),
+                        ),
+                        const SizedBox(height: 15,),
+                        Text(
+                          ManagerStrings.loginDescription,
+                          style: getBoldTextStyle(
+                            fontSize: ManagerFontSize.s18,
+                            color: ManagerColors.boldGrey,
+                          ),
+                        ),
+                        const SizedBox(height: 30,),
+                        baseTextFormField(
+                          controller: controller.email,
+                          hintText: ManagerStrings.email,
+                          keyboardType: TextInputType.emailAddress,
+                          validator: (value) => _failedValidator.validateEmail(
+                            value,
+                          ),
+                        ),
+                        const SizedBox(height: 25,),
+                        baseTextFormField(
+                          controller: controller.password,
+                          hintText: ManagerStrings.password,
+                          keyboardType: TextInputType.text,
+                          obscureText: true,
+                          validator: (value) => _failedValidator.validatePassword(
+                            value,
+                          ),
+                        ),
+                        const SizedBox(height: 20,),
+                        Row(
+                          children: [
+                            Checkbox(
+                              activeColor: ManagerColors.primaryColor,
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(
+                                  ManagerRadius.r4,
+                                ),
+                              ),
+                              value: true,
+                              onChanged: (status) {
+                                controller.changeRememberMe(status!);
+                              },
+                            ),
+                            Text(
+                              ManagerStrings.rememberMe,
+                              style: getBoldTextStyle(
+                                fontSize: ManagerFontSize.s14,
+                                color: ManagerColors.black,
+                              ),
+                            ),
+                            const Spacer(),
+                            Text(
+                              ManagerStrings.forgotPassword,
+                              style: getBoldTextStyle(
+                                fontSize: ManagerFontSize.s14,
+                                color: ManagerColors.primaryColor,
+                              ),
+                            ),
+                            const SizedBox(width: 10,)
+                          ],
+                        ),
+                        SizedBox(height: ManagerHeight.h70),
+                        mainButton(
+                            child: Text(
+                              ManagerStrings.login,
+                              style: getBoldTextStyle(
+                                fontSize: ManagerFontSize.s18,
+                                color: ManagerColors.white,
+                              ),
+                            ),
+                            minWidth: double.infinity,
+                            color: ManagerColors.primaryColor,
+                            height: ManagerHeight.h44,
+                            onPressed: () {
+                              if(controller.formKey.currentState!.validate()){
+                                controller.login(context);
+                              }
+                            }
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Text(
+                              ManagerStrings.notHaveAccount,
+                              style: getBoldTextStyle(
+                                  fontSize: ManagerFontSize.s14,
+                                  color: ManagerColors.black),
+                            ),
+                            mainButton(
+                                child: Text(
+                                  ManagerStrings.signup,
+                                  style: getBoldTextStyle(
+                                    fontSize: ManagerFontSize.s14,
+                                    color: ManagerColors.primaryColor,
+                                  ),
+                                ),
+                                onPressed: () {
+                                  // Get.offAllNamed(Routes.registerView);
+                                }),
+                          ],
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        );
+      },
+    );
   }
 }
