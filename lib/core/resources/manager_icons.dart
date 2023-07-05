fc0eb3adf8420790ae6ba2b7af94c1d867378c31
mstfa_201 <mostafaa.sh2001@gmail.com>
2023-07-05 15:23:00 +0300

    add resources

---
+++ lib/core/resources/manager_icons.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:home_service_application/core/resources/manager_colors.dart';
+
+class ManagerIcons{
+  static const Icon favorite = Icon(Icons.favorite_border,size: 20.0,color: ManagerColors.white);
+  static const Icon home = Icon(Icons.home);
+  static const Icon categories = Icon(Icons.apps);
+  static const Icon favorites = Icon(Icons.favorite);
+  static const Icon settings = Icon(Icons.settings);
+
+}
