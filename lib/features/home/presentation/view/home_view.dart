e03ecc3b394de47b2f3ee8bb11ada64cfcb6442b
mstfa_201 <mostafaa.sh2001@gmail.com>
2023-07-06 16:13:41 +0300

    edit in home feature

--- lib/features/home/presentation/view/home_view.dart
+++ lib/features/home/presentation/view/home_view.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:home_service_application/config/constants.dart';
 import 'package:home_service_application/core/resources/manager_fonts.dart';
 import 'package:home_service_application/core/resources/manager_styles.dart';
 import 'package:home_service_application/features/home/presentation/view/widget/custom_banner.dart';
@@ -10,6 +9,7 @@ import '../../../../core/resources/manager_sizes.dart';
 import '../../../../core/resources/manager_strings.dart';
 import '../../../../core/widgets/will_pop_scope.dart';
 import '../controller/home_controller.dart';
+import 'package:shimmer/shimmer.dart';
 
 
 class HomeView extends StatelessWidget {
@@ -17,43 +17,61 @@ class HomeView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return willPopScope(
-      child: Scaffold(
-        backgroundColor: ManagerColors.white,
-        appBar: AppBar(
-          title: Text(ManagerStrings.salla,style: getBoldTextStyle(fontSize: ManagerFontSize.s20, color: ManagerColors.black),),
-          elevation: Constants.elevationAppBar,
-          actions: const[ Padding(
-
-            padding: EdgeInsets.only(right: 20.0),
-            child: Icon(Icons.search_rounded,color: ManagerColors.black,),
-          )],
-          backgroundColor: ManagerColors.white,
-        ),
-        body: GetBuilder<HomeController>(
-          builder: (controller) {
-            return ListView(
-              children: [
-                SizedBox(height: ManagerHeight.h20),
-                const CustomBanner(),
-                SizedBox(height: ManagerHeight.h20),
-                Container(
-                  width: double.infinity,
-                  color: ManagerColors.lightGrey,
-                  child: Padding(
-                    padding: const EdgeInsets.only(left:8.0),
-                    child: Text(
-                      ManagerStrings.newProducts,
-                      style: getBoldTextStyle(fontSize: ManagerFontSize.s22, color: ManagerColors.black)
+    return GetBuilder<HomeController>(
+      builder: (controller) {
+        return willPopScope(
+          child: Scaffold(
+            backgroundColor: ManagerColors.white,
+            body: ListView(
+                  children: [
+                    SizedBox(height: ManagerHeight.h20),
+                    controller.homeDataModel.isNull?
+                    SizedBox(
+                      width: double.infinity,
+                      height: ManagerHeight.h160,
+                      child: Shimmer.fromColors(
+                        baseColor: ManagerColors.white,
+                        highlightColor: ManagerColors.lightGrey,
+                        child: Container(
+                          width: double.infinity,
+                          color: ManagerColors.grey,
+                        ),
+                      ),
+                    ):
+                    const CustomBanner(),
+                    SizedBox(height: ManagerHeight.h20),
+                    Container(
+                      width: double.infinity,
+                      color: ManagerColors.lightGrey,
+                      child: Padding(
+                        padding: const EdgeInsets.only(left:8.0),
+                        child: Text(
+                          ManagerStrings.newProducts,
+                          style: getBoldTextStyle(fontSize: ManagerFontSize.s22, color: ManagerColors.black)
+                        ),
+                      ),
                     ),
-                  ),
+                    controller.homeDataModel.isNull?
+                    SizedBox(
+                      width: double.infinity,
+                      height: ManagerHeight.h600,
+                      child: Shimmer.fromColors(
+                        baseColor: ManagerColors.white,
+                        highlightColor: ManagerColors.lightGrey,
+                        child: Container(
+                          width: double.infinity,
+                          color: ManagerColors.grey,
+                        ),
+                      ),
+                    ):
+                    const CustomProducts(),
+                  ],
                 ),
-                const CustomProducts(),
-              ],
-            );
-          },
-        ),
-      ),
+        )
+        );
+      }
     );
-  }
+          }
+
+
 }
