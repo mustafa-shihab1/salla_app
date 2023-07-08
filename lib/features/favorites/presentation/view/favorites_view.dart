d4dc731cb6cddb8c844ee408c8c07fc3a3431456
mstfa_201 <mostafaa.sh2001@gmail.com>
2023-07-08 23:24:01 +0300

    edit favorites view

--- lib/features/favorites/presentation/view/favorites_view.dart
+++ lib/features/favorites/presentation/view/favorites_view.dart
@@ -1,17 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:home_service_application/core/resources/manager_fonts.dart';
-import 'package:home_service_application/core/resources/manager_styles.dart';
-import 'package:home_service_application/features/category/presentation/view/widget/category_item.dart';
-import 'package:home_service_application/features/favorites/presentation/view/widget/favorite_item.dart';
+import 'package:home_service_application/features/home/domain/model/products_model.dart';
 import 'package:home_service_application/features/home/presentation/controller/home_controller.dart';
-import 'package:home_service_application/features/home/presentation/view/widget/custom_banner.dart';
-import 'package:home_service_application/features/home/presentation/view/widget/custom_products.dart';
 import '../../../../core/resources/manager_colors.dart';
 import '../../../../core/resources/manager_sizes.dart';
-import '../../../../core/resources/manager_strings.dart';
-import '../../../../core/widgets/will_pop_scope.dart';
-import 'package:shimmer/shimmer.dart';
 
 
 class FavoritesView extends StatelessWidget {
@@ -22,13 +14,39 @@ class FavoritesView extends StatelessWidget {
     return GetBuilder<HomeController>(
         builder: (controller) {
           return ListView.separated(
-            itemBuilder: (context, index) => favoritesItem(model: controller.homeDataModel!.products![index],context: context),
+            itemBuilder: (context, index) => Padding(
+              padding: const EdgeInsets.all(20.0),
+              child: Row(
+                children: [
+                  Image(
+                    image:NetworkImage('${controller.images[index]}'),
+                    width: ManagerWidth.w100,
+                    height: ManagerHeight.h100,
+                    fit: BoxFit.cover,
+                  ),
+                  const SizedBox(
+                    width: 15,
+                  ),
+                  Flexible(
+                    child: Text(
+                     '${controller.names[index]} ',
+                      style: const TextStyle(
+                          overflow: TextOverflow.ellipsis,
+                          height: 1.3,
+                      ),
+                      maxLines: 4,
+                    ),
+                  ),
+                  const SizedBox(width: 10,),
+                  const Icon(Icons.arrow_forward_ios_outlined,size: 20,),
+                ],
+              ),
+            ),
             separatorBuilder: (BuildContext context, int index) =>Padding(
               padding: const EdgeInsets.only(left: 20.0,top: 5.0,bottom: 5.0),
               child: Container(width: double.infinity,color: ManagerColors.lightGrey,height: 1.0,),
             ),
-            itemCount: controller.favorits.length ,
-
+            itemCount: controller.names.length ,
           );
         }
     );
