7f0f3c9e231d366481909dc27de65f69f2b22af9
mstfa_201 <mostafaa.sh2001@gmail.com>
2023-07-08 23:12:36 +0300

    edit category_item

--- lib/features/category/presentation/view/widget/category_item.dart
+++ lib/features/category/presentation/view/widget/category_item.dart
@@ -14,30 +14,27 @@ Widget categoryItem({
     GetBuilder<CategoryController>(builder: (context) {
       return Padding(
         padding: const EdgeInsets.all(20.0),
-        child: Expanded(
+        child: Row(
+          children: [
+            Image(
+              image: NetworkImage('${model.image}'),
+              width: ManagerWidth.w100,
+              height: ManagerHeight.h100,
+              fit: BoxFit.cover,
+            ),
+            const SizedBox(
+              width: 15,
+            ),
 
-          child: Row(
-            children: [
-              Image(
-                image: NetworkImage('${model.image}'),
-                width: ManagerWidth.w100,
-                height: ManagerHeight.h100,
-                fit: BoxFit.cover,
-              ),
-              const SizedBox(
-                width: 15,
-              ),
+            Text(
+              '${model.name}',
+              style: getBoldTextStyle(fontSize: ManagerFontSize.s18, color: ManagerColors.black),
+            ),
+            const Spacer(),
+            IconButton(onPressed: (){
 
-              Text(
-                '${model.name}',
-                style: getBoldTextStyle(fontSize: ManagerFontSize.s18, color: ManagerColors.black),
-              ),
-              const Spacer(),
-              IconButton(onPressed: (){
-
-              }, icon: const Icon(Icons.arrow_forward_ios_outlined,size: 20,))
-            ],
-          ),
+            }, icon: const Icon(Icons.arrow_forward_ios_outlined,size: 20,))
+          ],
         ),
       );
     });
