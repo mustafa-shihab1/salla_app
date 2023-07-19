bc61a35a7f3204af7e500320c599dfa42a8c7d52
mstfa_201 <mostafaa.sh2001@gmail.com>
2023-07-19 14:38:09 +0300

    profile model

---
+++ lib/features/profile/domain/model/profile_data_model.dart
@@ -0,0 +1,19 @@
+class ProfileDataModel {
+  int? id;
+  String? name;
+  String? email;
+  String? token;
+  String? phone;
+  String? image;
+
+
+  ProfileDataModel({
+    this.id,
+    this.name,
+    this.email,
+    this.token,
+    this.phone,
+    this.image,
+  });
+
+}
