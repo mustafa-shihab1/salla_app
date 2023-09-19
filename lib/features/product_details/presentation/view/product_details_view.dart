9d8998dcc63b616934244bf84121f5aa3c5cf422
mstfa_201 <mostafaa.sh2001@gmail.com>
2023-09-19 09:55:07 +0300

    fix product-details bug

--- lib/features/product_details/presentation/view/product_details_view.dart
+++ lib/features/product_details/presentation/view/product_details_view.dart
@@ -1,18 +1,27 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:home_service_application/core/resources/manager_colors.dart';
+import 'package:home_service_application/core/resources/manager_fonts.dart';
+import 'package:home_service_application/core/resources/manager_icons.dart';
 import 'package:home_service_application/core/resources/manager_sizes.dart';
+import 'package:home_service_application/core/resources/manager_strings.dart';
+import 'package:home_service_application/core/resources/manager_styles.dart';
+import 'package:home_service_application/core/widgets/main_button.dart';
+import 'package:home_service_application/features/home/domain/model/products_model.dart';
 import 'package:home_service_application/features/home/presentation/controller/home_controller.dart';
+import 'package:home_service_application/features/product_details/presentation/view/widget/custom_description.dart';
 import 'package:home_service_application/features/product_details/presentation/view/widget/cutom_image.dart';
 
 class ProductDetailsView extends StatelessWidget {
-  const ProductDetailsView({Key? key}) : super(key: key);
+  const ProductDetailsView({Key? key,required this.productsModel}) : super(key: key);
+  final ProductsModel productsModel;
 
   @override
   Widget build(BuildContext context) {
     return GetBuilder<HomeController>(
         builder: (controller) {
       return Scaffold(
+        backgroundColor: ManagerColors.white,
         appBar: AppBar(
           elevation: 0,
           backgroundColor: ManagerColors.transparent,
@@ -23,38 +32,58 @@ class ProductDetailsView extends StatelessWidget {
         ),
         body: Padding(
           padding:const  EdgeInsets.symmetric(horizontal: 20.0),
-          child: Column(
-            children: [
-              SizedBox(height: ManagerHeight.h20),
-             const CustomImage(),
+          child: SingleChildScrollView(
+            child: Column(
+              children: [
+               Stack(
+                 alignment: AlignmentDirectional.topEnd,
+                 children: [
+                   CustomImage(productsModel: productsModel),
+                   IconButton(
+                     onPressed: () {
+                       controller.changeFavorites(productsModel);
+                     },
+                     iconSize: ManagerIconSize.s40,
+                     icon: CircleAvatar(
+                         radius: 30.0,
+                         backgroundColor: productsModel.in_favorites! ? ManagerColors.primaryColor:ManagerColors.grey,
+                         child: Icon(
+                           ManagerIcons.favorite,
+                           color: ManagerColors.white,
+                           size: ManagerIconSize.s22,
+                         )
+                     ),
+                   ),
+                 ],
+               ),
 
-              // SizedBox(height: ManagerHeight.h20),
-              // CustomDescription(
-              //   name: controller.homeDataModel!.products![2].name!,
-              //   description:
-              //       controller.homeDataModel!.products![2].description!,
-              //   price: controller.homeDataModel!.products![2].price!,
-              //   onRatingUpdate: (rating) {},
-              // ),
-              // SizedBox(height: ManagerHeight.h20),
-              // mainButton(
-              //     padding: EdgeInsets.symmetric(vertical: ManagerHeight.h16),
-              //     child: Text(
-              //       ManagerStrings.addToCard,
-              //       style: getBoldTextStyle(
-              //         fontSize: ManagerFontSize.s22,
-              //         color: ManagerColors.white,
-              //       ),
-              //     ),
-              //     minWidth: double.infinity,
-              //     color: ManagerColors.primaryColor,
-              //     height: ManagerHeight.h44,
-              //     onPressed: () {
-              //     }
-              // ),
-              // SizedBox(height: ManagerHeight.h20),
-
-            ],
+                SizedBox(height: ManagerHeight.h20),
+                CustomDescription(
+                  name: productsModel.name!,
+                  description:
+                      productsModel.description!,
+                  price: productsModel.price!,
+                  onRatingUpdate: (rating) {},
+                ),
+                SizedBox(height: ManagerHeight.h20),
+                mainButton(
+                    padding: EdgeInsets.symmetric(vertical: ManagerHeight.h16),
+                    child: Text(
+                      ManagerStrings.addToCart,
+                      style: getBoldTextStyle(
+                        fontSize: ManagerFontSize.s22,
+                        color: ManagerColors.white,
+                      ),
+                    ),
+                    minWidth: double.infinity,
+                    color: ManagerColors.primaryColor,
+                    height: ManagerHeight.h44,
+                    onPressed: () {
+                    }
+                ),
+                SizedBox(height: ManagerHeight.h20),
+              ],
+            ),
           ),
         ),
       );
