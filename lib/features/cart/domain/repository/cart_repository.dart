863f79bfd4f895e4158c1412ff868ce4315729c4
mstfa_201 <mostafaa.sh2001@gmail.com>
2023-09-21 23:48:01 +0300

    repository

--- lib/features/cart/domain/repository/cart_repository.dart
+++ lib/features/cart/domain/repository/cart_repository.dart
@@ -1,7 +1,11 @@
 import 'package:dartz/dartz.dart';
 import 'package:home_service_application/core/error_handler/error_handler.dart';
+import 'package:home_service_application/features/cart/data/request/cart_request.dart';
+import 'package:home_service_application/features/cart/domain/model/add_remove_cart_data_model.dart';
+import 'package:home_service_application/features/cart/domain/model/add_remove_cart_model.dart';
 import 'package:home_service_application/features/cart/domain/model/cart_model.dart';
 
 abstract class CartRepository {
   Future<Either<Failure, CartModel>> getCart();
+  Future<Either<Failure, AddRemoveCartModel>> addRemove(CartRequest cartRequest);
 }
